01edc0e06cddc44db38ff61bccd652168c3a22b7
Matias Montecinos <montecinosmatias142@gmail.com>
2023-10-13 17:43:52 -0300

    cambio

--- nota_alumnos_clases/main.py
+++ nota_alumnos_clases/main.py
@@ -1,37 +1,42 @@
 from colegio.profesor import Profesor
 from colegio.estudiante import Estudiante
 
-usuario = input("ingresa el usuario para entrar: ")
+opcion = input("eres el profesor o alumno ? ").lower()
 
-contraseña = input("ingresa la contraseña : ")
+if opcion == "profesor":
+    usuario = input("ingresa el usuario para entrar: ")
+    contraseña = input("ingresa la contraseña : ")
+    nota_asignada = float(input("ingresa  la nota"))
+    profe_matias = Profesor("matias",nota_asignada,usuario,contraseña)
+    profe_matias.login_profesor()
+    profe_matias.promedio_notas()
+# =====================================
+elif opcion == "alumno":
+    alumno_matias = Estudiante("matias" , "montecinos",3.5)
+    alumno_nico = Estudiante("nicolas" , "ojeda",6.6)
+    alumno_kevin = Estudiante("kevin" , "ibarra",6.1)
+    alumno_lucas = Estudiante("lucas" , "montecinos",5.0)
+    alumno_benja = Estudiante("benjamin" , "Sotomayor",2.0)
 
-profe_matias = Profesor("matias",3.5,usuario,contraseña)
+    alumno_matias.login_estudiante()
+    alumno_matias.carrera()
+    alumno_matias.promedio_notas()
 
-profe_matias.login_profesor()
+    alumno_nico.login_estudiante()
+    alumno_nico.carrera()
+    alumno_nico.promedio_notas()
 
-alumno_matias = Estudiante("matias" , "montecinos",3.5)
-alumno_nico = Estudiante("nicolas" , "ojeda",6.6)
-alumno_kevin = Estudiante("kevin" , "ibarra",6.1)
-alumno_lucas = Estudiante("lucas" , "montecinos",5.0)
-alumno_benja = Estudiante("benjamin" , "Sotomayor",2.0)
+    alumno_kevin.login_estudiante()
+    alumno_kevin.carrera()
+    alumno_kevin.promedio_notas()
 
-alumno_matias.login_estudiante()
-alumno_matias.carrera()
-alumno_matias.promedio_notas()
+    alumno_lucas.login_estudiante()
+    alumno_lucas.carrera()
+    alumno_lucas.promedio_notas()
 
-alumno_nico.login_estudiante()
-alumno_nico.carrera()
-alumno_nico.promedio_notas()
+    alumno_benja.login_estudiante()
+    alumno_benja.carrera()
+    alumno_benja.promedio_notas()
 
-
-alumno_kevin.login_estudiante()
-alumno_kevin.carrera()
-alumno_kevin.promedio_notas()
-
-alumno_lucas.login_estudiante()
-alumno_lucas.carrera()
-alumno_lucas.promedio_notas()
-
-alumno_benja.login_estudiante()
-alumno_benja.carrera()
-alumno_benja.promedio_notas()
+else:
+    print("no eres ni alumno ni profesor no puedes seguir adelante ")
